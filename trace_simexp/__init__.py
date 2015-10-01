dea56fa3872d0cfa9bbe8f6ff7eb0a4db525981f
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-01 17:19:39 +0200

    Add bare `prepro` package to carry out the preprocess phase
     - `rescaled_design.py`: module to rescale the normalized design matrix
        according to the specified range and distribution in the list of
        parameters file
     - `tracin_template.py": module to create tracin template file, a modified
       base case tracin where all the parameters specified in the list of
       parameters file was changed with key to be substituted later
     - `parse_param_file.py`: module with utilities to parse the list of
       parameters file

---
+++ trace_simexp/__init__.py
@@ -0,0 +1,5 @@
+"""Module with utilities to carry out simulation experiment in TRACE code
+"""
+from . import prepro
+
+__author__ = "Damar Wicaksono"
---
+++ trace_simexp/prepro/__init__.py
@@ -0,0 +1,6 @@
+"""Module to preprocessed the input files and make spawned tracin
+"""
+from . import rescaled_design
+from . import tracin_template
+
+__author__ = "Damar Wicaksono"
