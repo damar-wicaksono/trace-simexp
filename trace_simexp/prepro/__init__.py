9394bfe82f5a7239c4a79bfa509ef5174ceb8c7f
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-01 17:56:50 +0200

    Add rescale.py; module to rescale univariate random samples

--- trace_simexp/prepro/__init__.py
+++ trace_simexp/prepro/__init__.py
@@ -1,6 +1,6 @@
 """Package to carry out preprocessing phase of the simulation experiment
 """
-from . import rescaled_design
+from . import rescale_design
 from . import tracin_template
 
 __author__ = "Damar Wicaksono"
---
+++ trace_simexp/prepro/rescale.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Module with functions to scale a univariate [0, 1] samples to a specified
+distribution
+"""
+
+__author__ = "Damar Wicaksono"
