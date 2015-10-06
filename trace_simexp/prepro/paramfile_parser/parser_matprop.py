03370936014e28fd3470a9a72c1b227912c81c8a
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-06 09:43:05 +0200

    Rename and restructure parse_matprop module
    Now it is located under `paramfile_parser` package (package to
    parse list of parameters file)

--- trace_simexp/prepro/paramfile_parser/parser_matprop.py
+++ trace_simexp/prepro/paramfile_parser/parser_matprop.py
@@ -4,7 +4,7 @@
 __author__ = "Damar Wicaksono"
 
 
-def parse_matprop(line, params_dict, verbose=True):
+def parse(line, params_dict, verbose=True):
     r"""Parse material property specification from a list of parameters file
 
     note that the input argument `params_dict` is mutable and will be modified
