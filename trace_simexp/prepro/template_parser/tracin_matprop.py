02a908f866e2d2f89ee61a7e5db46884eaab52a8
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-06 11:53:28 +0200

    Add bare function definition for get_nom_val()

--- trace_simexp/prepro/template_parser/tracin_matprop.py
+++ trace_simexp/prepro/template_parser/tracin_matprop.py
@@ -2,3 +2,13 @@
 """
 
 __author__ = "Damar Wicaksono"
+
+
+def get_nom_val(tracin_lines, param_dict):
+    r"""Get the nominal value of material property parameter from tracin base
+
+    :param tracin_lines: (list of str) the tracin base as a list
+    :param param_dict: (dict) the dictionary of the matprop parameter
+    :return: (array) the nominal values of the matprop parameter
+    """
+    return None
