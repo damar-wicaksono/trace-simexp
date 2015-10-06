e497ab143e2c47e38fce532c3029b5c1f04633cb
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-06 11:45:11 +0200

    Read tracin base and get nominal values for senscoef parameters

--- trace_simexp/prepro/template_parser/tracin_senscoef.py
+++ trace_simexp/prepro/template_parser/tracin_senscoef.py
@@ -1,3 +1,28 @@
 """Module with functionalities to parse tracin base for sensitivity coefficient
 """
-__author__ = 'wicaksono_d'
+
+__author__ = "Damar Wicaksono"
+
+
+def get_nom_val(tracin_lines, param_dict):
+    r"""Get the nominal values of sensitivity coefficient from tracin base
+
+    :param tracin_lines: (list of str) the tracin base in python list
+    :param param_dict: (dict) the dictionary of senscoef parameter
+    :returns: (float) the nominal value of the sensitivity coefficients
+    """
+
+    nom_val = None
+
+    # loop over tracin lines
+    for line_num, tracin_line in enumerate(tracin_lines):
+
+        var_num = tracin_line.split()[0]            # safer to keep it as string
+        if var_num == str(param_dict["var_num"]):
+            # the sensitivity coefficient identifier is the beginning of line
+            nom_val = float(tracin_line.split()[2])
+        else:
+            continue
+
+    return nom_val
+
