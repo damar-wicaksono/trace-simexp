94a0fbf999fa7ffb46bf373a27656b8dd2fc9a56
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-06 15:12:05 +0200

    Fix minor typo on comment

--- trace_simexp/prepro/template_parser/tracin_matprop.py
+++ trace_simexp/prepro/template_parser/tracin_matprop.py
@@ -1,4 +1,4 @@
-"""Module with funtionalities to parse tracin base for material property
+"""Module with functionalities to parse tracin base for material property
 """
 
 __author__ = "Damar Wicaksono"
