437f7e62cb205026f19d4f5e054d58eee24f04d5
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-12 17:05:03 +0200

    Add function to create key and replace the nominal value with it

--- trace_simexp/prepro/template_parser/tracin_senscoef.py
+++ trace_simexp/prepro/template_parser/tracin_senscoef.py
@@ -21,8 +21,37 @@ def get_nom_val(tracin_lines, param_dict):
         if var_num == str(param_dict["var_num"]):
             # the sensitivity coefficient identifier is the beginning of line
             nom_val = float(tracin_line.split()[2])
+            break
         else:
             continue
 
     return nom_val
 
+
+def put_key(tracin_lines, param_dict):
+    r"""Function to replace the nominal value of senscoef parameters with key
+
+    :param tracin_lines: (list of string) the base tracin in a python list
+    :param param_dict: (dict) the dictionary of senscoef parameter
+    :return: (list of str) the base tracin with line(s) modified according to
+        the senscoef parameter key
+    """
+     # loop over tracin lines
+    for line_num, tracin_line in enumerate(tracin_lines):
+
+        var_num = tracin_line.split()[0]            # safer to keep it as string
+        if var_num == str(param_dict["var_num"]):
+            # the sensitivity coefficient identifier is the beginning of line
+            card = tracin_line.split()
+            # Create the key and replace the word in the card
+            card[2] = "${}_{}" .format(param_dict["data_type"],
+                                       param_dict["var_num"])
+            # Sensitivity coefficient always have 3 cards
+            card = "{:<8s}{:1s}{:>16s} " .format(card[0], card[1], card[2])
+            # replace the line in tracin with the modified line
+            tracin_lines[line_num] = card
+            break
+        else:
+            continue
+
+    return tracin_lines
