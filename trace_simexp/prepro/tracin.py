235ac05d86247e686e899fec2f709578ed727636
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-14 17:06:27 +0200

    Add tracin.create() function to generate a set of sampled perturbation factors

--- trace_simexp/prepro/tracin.py
+++ trace_simexp/prepro/tracin.py
@@ -7,14 +7,15 @@ __author__ = "Damar Wicaksono"
 def create(template_lines, params_dict, norm_pert_factors):
     r"""Function to create a trace input as string based on perturbed values
 
-    :param template_lines:
-    :param params_dict:
-    :param norm_pert_factors:
-    :return:
+    :param template_lines: (str.Template) the template based on base tracin
+    :param params_dict: (list of dict) the list of parameters in a dictionary
+    :param norm_pert_factors: (array) an array of normalized perturbed factor
+    :return: (str) a tracin with template keys substituted with the actual
+        perturbed model parameter values
     """
     from .tracin_generator import perturb
 
-    realization = dict()
+    perturb_dict = dict()
 
     # Loop over all parameters in the list of dictionaries
     for i, param in enumerate(params_dict):
@@ -23,11 +24,13 @@ def create(template_lines, params_dict, norm_pert_factors):
         rescaled_factor = perturb.rescale_perturb(param, norm_pert_factors[i])
 
         # Perturbed the model parameters
+        perturb_param = perturb.perturb_param(param, rescaled_factor)
 
+        # Create dictionary with keys correspond to template lines and perturbed
+        # model parameter values
+        perturb_dict.update(perturb.create_dict(param, perturb_param))
 
-    # Create dictionary with keys correspond to template lines and values from
-    # rescaled perturbed values
     # replace the key in template with perturbed values
-    # return the perturbed tracin string
-    # done
+    tracin = template_lines.substitute(perturb_dict)
 
+    return tracin
