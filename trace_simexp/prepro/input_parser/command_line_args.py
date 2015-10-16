8909e88f428a3ebc898c9f895490acb73b74bad5
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-16 12:14:50 +0200

    Reorganize package to parse input parameters

---
+++ trace_simexp/prepro/input_parser/command_line_args.py
@@ -0,0 +1,84 @@
+"""Module to parse command line input arguments
+"""
+
+__author__ = "Damar Wicaksono"
+
+
+def get():
+    r"""Parse input arguments required for preprocessing phase
+
+    :return:
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="trace_simexp Preprocessor - Create tracin and dirtree"
+    )
+
+    # Select which samples to run
+    parser.add_argument(
+        "-ns", "--num_samples",
+        type=int,
+        nargs="+",
+        help="Samples to run",
+        required=False
+    )
+
+    # Select a range of samples to run
+    parser.add_argument(
+        "-nr", "--num_range",
+        type=int,
+        nargs=2,
+        help="Range of samples to run",
+        required=False
+    )
+
+    # The base directory name
+    parser.add_argument(
+        "-b", "--base_name",
+        type=str,
+        help="The base directory name",
+        default="./simulation",
+        required=False
+    )
+
+    # The base tracin filename
+    parser.add_argument(
+        "-tracin", "--base_tracin",
+        type=str,
+        help="The base tracin filename",
+        required=True
+    )
+
+    # The base design matrix filename
+    parser.add_argument(
+        "-dm", "--design_matrix",
+        type=str,
+        help="The design matrix filename",
+        required=True
+    )
+
+    # The list of parameter filename
+    parser.add_argument(
+        "-parlist", "--params_list",
+        type=str,
+        help="The list of parameters filename",
+        required=True
+    )
+
+    # Get the command line arguments
+    args = parser.parse_args()
+
+    # Check if any sample is specified
+    if args.num_samples is None and args.num_range is None:
+        parser.error("either -ns or -nr has to be present!")
+    elif args.num_samples is not None and args.num_range is not None:
+        parser.error("-ns or -nr cannot both be present!")
+
+    if args.num_samples is not None:
+        return args.num_samples, args.base_name, args.base_tracin,\
+               args.design_matrix, args.params_list
+    elif args.num_range is not None:
+        return args.num_range, args.base_name, args.base_tracin,\
+               args.design_matrix, args.params_list
+
