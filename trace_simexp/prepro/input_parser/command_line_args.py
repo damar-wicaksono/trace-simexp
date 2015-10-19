7f3f0cbf19f710bcd92502132a5ffe922bab754f
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-19 14:16:19 +0200

    Add command line arguments check

--- trace_simexp/prepro/input_parser/command_line_args.py
+++ trace_simexp/prepro/input_parser/command_line_args.py
@@ -136,3 +136,59 @@ def get():
         return args.all_samples, args.base_name, args.base_tracin,\
                args.design_matrix, args.params_list, args.overwrite, \
                args.info
+
+
+def check(inputs):
+    """
+
+    :param inputs:
+    :return:
+    """
+    import os
+    import numpy as np
+
+    # Check if the base tracin exists
+    if not os.path.exists(inputs["tracin_base_file"]):
+        raise ValueError("The base tracin file does not exist!")
+    else:
+        pass
+
+    # Check if design matrix file exist
+    if os.path.exists(inputs["dm_file"]):
+        num_params_dm = np.loadtxt(inputs["dm_file"]).shape[1]
+        num_samples = np.loadtxt(inputs["dm_file"]).shape[0]
+    else:
+        raise ValueError("The design matrix file does not exists!")
+
+    # Check if list of parameters file exist
+    if os.path.exists(inputs["params_list_file"]):
+        with open(inputs["params_list_file"], "rt") as params_list_file:
+            params_list_line = params_list_file.readlines()
+        num_params_list_file = 0
+        for i in params_list_line:
+            if not i.startswith("#"):
+                num_params_list_file += 1
+    else:
+        raise ValueError("The list of parameters file does not exist!")
+
+    # Check the number of parameters in the design matrix and list of parameters
+    if num_params_list_file != num_params_dm:
+        raise ValueError("The number of parameters is inconsistent\n"
+                         "{:10d} in {} and {:10d} in {}"
+                         .format(num_params_list_file,
+                                 inputs["params_list_name"],
+                                 num_params_dm,
+                                 inputs["dm_name"]))
+    else:
+        pass
+
+    # Check if the sample number asked is available
+    if not isinstance(inputs["samples"], bool):
+        if max(inputs["samples"]) > num_samples:
+            raise ValueError("The sample asked is beyond the available "
+                             "samples\n {:10d} asked and {:10d} in {}"
+                         .format(max(inputs["samples"]),
+                                 num_samples,
+                                 inputs["dm_name"]))
+    else:
+        pass
