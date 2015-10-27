3db87776c433804bfa2a439a7027e560b796b23b
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-27 15:34:03 +0100

    Add sample specification as part of command line arguments

--- trace_simexp/execute/input_parser/command_line_args.py
+++ trace_simexp/execute/input_parser/command_line_args.py
@@ -32,6 +32,33 @@ def get():
         required=False
     )
 
+    # Select which samples to run
+    parser.add_argument(
+        "-ns", "--num_samples",
+        type=int,
+        nargs="+",
+        help="Samples to run",
+        required=False
+    )
+
+    # Select a range of samples to run
+    parser.add_argument(
+        "-nr", "--num_range",
+        type=int,
+        nargs=2,
+        help="Range of samples to run",
+        required=False
+    )
+
+    # Select all of the available samples
+    parser.add_argument(
+        "-as", "--all_samples",
+        action="store_true",
+        help="Run all samples",
+        default=False,
+        required=False
+    )
+
     # The fullname of the scratch directory
     parser.add_argument(
         "-scratch", "--scratch_directory",
@@ -59,8 +86,50 @@ def get():
     # Get the command line arguments
     args = parser.parse_args()
 
-    return args.info_file, args.num_processors, args.scratch_directory, \
-           args.trace_executable, args.xtv2dmx_executable
+    # Check if any sample is specified and each are mutually exclusive
+    if args.num_samples is None and args.num_range is None \
+            and not args.all_samples:
+        parser.error("Either -ns, -nr, or -as has to be present!")
+    elif args.num_samples is not None and args.num_range is not None:
+        parser.error("-ns or -nr cannot both be present!")
+    elif args.num_samples is not None and args.all_samples:
+        parser.error("-ns or -as cannot both be present!")
+    elif args.num_range is not None and args.all_samples:
+        parser.error("-nr or -as cannot both be present!")
+    else:
+        pass
+
+
+    # Sample has to be specified
+    # Select individual samples
+    if args.num_samples is not None:
+        # Sample number has to be positive
+        if True in [_ < 0 for _ in args.num_samples]:
+            parser.error(
+                "Number of samples with -ns has to be strictly positive!")
+        else:
+             return args.num_samples, args.info_file, args.num_processors, \
+                    args.scratch_directory, args.trace_executable, \
+                    args.xtv2dmx_executable
+
+    # Use range of samples
+    elif args.num_range is not None:
+        # Sample range number has to be positive
+        if (args.num_range[0] <= 0 or args.num_range[1] <= 0) and \
+                (args.num_range[0] > args.num_range[1]):
+            parser.error("Sample range with -nr has to be strictly positive!"
+                         "and the first is smaller than the second")
+        else:
+            samples = list(range(args.num_range[0], args.num_range[1]+1))
+            return samples, args.info_file, args.num_processors, \
+                   args.scratch_directory, args.trace_executable, \
+                   args.xtv2dmx_executable
+
+    # Select all samples
+    elif args.all_samples is not None:
+        return args.all_samples, args.info_file, args.num_processors, \
+               args.scratch_directory, args.trace_executable, \
+               args.xtv2dmx_executable
 
 
 def check(inputs):
