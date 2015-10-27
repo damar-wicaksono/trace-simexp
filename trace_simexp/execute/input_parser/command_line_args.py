fe2f2e43e7db506cd1b1e5e6df3c38b865201d41
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-27 14:13:20 +0100

    Update the get() function to obtain the command line arguments

--- trace_simexp/execute/input_parser/command_line_args.py
+++ trace_simexp/execute/input_parser/command_line_args.py
@@ -9,6 +9,58 @@ def get():
 
     :return:
     """
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="trace_simexp Execute - Run all preprocessed trace inputs"
+    )
+
+    # The fullname of info_file from the pre-processing phase
+    parser.add_argument(
+        "-info", "--info_file",
+        type=str,
+        help="The pre-processing phase info file",
+        required=True
+    )
+
+    # The number of processors
+    parser.add_argument(
+        "-nprocs", "--num_processors",
+        type=int,
+        help="The number of available processors",
+        default=1,
+        required=False
+    )
+
+    # The fullname of the scratch directory
+    parser.add_argument(
+        "-scratch", "--scratch_directory",
+        type=str,
+        help="The scratch directory",
+        required=False
+    )
+
+    # The trace executable
+    parser.add_argument(
+        "-trace", "--trace_executable",
+        type=str,
+        help="The trace executable",
+        required=True
+    )
+
+    # The xtv2dmx executable
+    parser.add_argument(
+        "-xtv2dmx", "--xtv2dmx_executable",
+        type=str,
+        help="The xtv2dmx executable",
+        required=True
+    )
+
+    # Get the command line arguments
+    args = parser.parse_args()
+
+    return args.info_file, args.num_processors, args.scratch_directory, \
+           args.trace_executable, args.xtv2dmx_executable
 
 
 def check(inputs):
