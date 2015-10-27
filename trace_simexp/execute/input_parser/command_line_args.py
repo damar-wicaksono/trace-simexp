0256dc6b0d1fa01c508c597823a3fc56f79db7b3
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-10-27 13:17:12 +0100

    Add bare modules of execute phase input parser

--- trace_simexp/execute/input_parser/command_line_args.py
+++ trace_simexp/execute/input_parser/command_line_args.py
@@ -1,1 +1,18 @@
-__author__ = 'wicaksono_d'
+"""Module to parse command line input arguments - Execute phase
+"""
+
+__author__ = "Damar Wicaksono"
+
+
+def get():
+    r"""Get the command line arguments of the execute phase
+
+    :return:
+    """
+
+
+def check(inputs):
+    r"""Check the validity of the inputs
+
+    :return:
+    """
--- trace_simexp/execute/input_parser/info_file.py
+++ trace_simexp/execute/input_parser/info_file.py
@@ -1,1 +1,12 @@
-__author__ = 'wicaksono_d'
+"""Module to read info file - Execute phase
+"""
+
+__author__ = "Damar Wicaksono"
+
+
+def read(info_filename):
+    r"""Read the info file produced in the preprocessing phase
+
+    :param info_filename: (str) the filename for the info_file
+    :return:
+    """
