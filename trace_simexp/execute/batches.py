249699a987a64eb6754d4b30315fe7160fc1a48b
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-11-03 11:52:06 +0100

    Add prototype to create a run_dirname

--- trace_simexp/execute/batches.py
+++ trace_simexp/execute/batches.py
@@ -16,6 +16,28 @@ def run(exec_inputs: dict):
     for batch_iter in create_iter(num_samples, exec_inputs["num_procs"]):
         create(batch_iter, exec_inputs)
 
+        # Create bunch of run directory names
+        run_dirname = "{}/{}/{}-{}/{}-{}" .format(
+            exec_inputs["base_dir"],
+            exec_inputs["case_name"],
+            exec_inputs["params_list_name"],
+            exec_inputs["dm_name"],
+            exec_inputs["case_name"],
+            i
+        )
+        # Create bunch of log files
+        # Create bunch of scratch directory names
+        # Create bunch of trace commands
+        # Execute TRACE commands
+        # Create bunch of xtv2dmx commands
+        # Execute xtv2dmx commands
+        # Create bunch of .dif files
+        # Create bunch of .tpr files
+        # Create bunch of .out files
+        # Create bunch of .ech files
+        # Create bunch of .msg files
+        # Clean up TRACE directories
+
 
 def create(batch_iterator: itertools.islice, exec_inputs: dict):
     r"""Create an iterator of total runs in batch size
@@ -24,11 +46,28 @@ def create(batch_iterator: itertools.islice, exec_inputs: dict):
     :param num_processors: (int) number of available processors / batch size
     :return: (iterator) an iterator
     """
+    log_files = []
+    run_dirnames = []
+    scratch_dirnames = []
+    trace_cmds = []
+    xtv2dmx_cmds = []
+
     # Iterate the passed iterator
     for i in batch_iterator:
-        print(i)
+
+        # Create a list of files
+        run_dirname = "{}/{}/{}-{}/{}-run"
+        run_scratch_dir = ""
+        run_filename = ""
+        run_dif = ""
+        run_ech = ""
     # Create List of
-    # Bunch of log files
+
+        # Bunch of log files
+        log_filename = ""
+        log_fullname = ""
+        log_files.append(log_fullname)
+
     # Bunch of working directories
     # Bunch of scratch directories
     # Bunch of TRACE execution commands
@@ -42,21 +81,35 @@ def create(batch_iterator: itertools.islice, exec_inputs: dict):
     # Bunch of TRACE .msg files
 
 
+def exec_trace(log_files: list, run_dirnames: list,
+               scratch_dirnames: list, trace_cmds: list,
+               xtv2dmx_cmds: list):
+    r"""Execute multiple TRACE input deck
 
-def exec_trace():
-    """Execute multiple
+    :return:
+    """
+    import subprocess
 
+
+def exec_xtv2dmx(scratch_dirnames: list, xtv2dmx_cmds: list):
+    r"""Convert xtv files to dmx file to save some space
+
+    :param scratch_dirnames:
+    :param xtv2dmx_cmds:
     :return:
     """
     return None
 
 
-def clean_trace():
+def clean_trace(scratch_xtvs: list, link_xtvs: list,
+                tpr_files: list, dif_files: list,
+                ech_files: list, msg_file: list,
+                out_files: list):
     """Clean TRACE Directories
 
     :return:
     """
-    return None
+    import subprocess
 
 
 def create_iter(num_samples: int, num_processors: int) -> itertools.islice:
