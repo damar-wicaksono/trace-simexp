3ebc74e850ef4577cb5aa28fc311aab6dfa7632f
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-11-20 17:54:31 +0100

    Add bare modules to parse command line arguments for each phases

---
+++ trace_simexp/cmdln_args/execute.py
@@ -0,0 +1,4 @@
+"""Module to parse command line arguments in execute phase
+"""
+
+__author__ = "Damar Wicaksono"
---
+++ trace_simexp/cmdln_args/prepro.py
@@ -0,0 +1,4 @@
+"""Module to parse command line arguments in pre-processing phase
+"""
+
+__author__ = "Damar Wicaksono"
