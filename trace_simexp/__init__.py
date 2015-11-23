dfbc9e4891096d66df8bafaa19f1ada93b41921f
WD41 <damar.wicaksono@psi.ch>
2015-11-23 11:17:18 +0100

    Remove unncecessary import statement

--- trace_simexp/__init__.py
+++ trace_simexp/__init__.py
@@ -1,11 +1,10 @@
 """Module with utilities to carry out simulation experiment in TRACE code
 """
 from . import prepro
-from . import pkg_execute
-
 from . import postpro
 from . import cmdln_args
 from . import info_file
 from . import util
+
 
 __author__ = "Damar Wicaksono"
