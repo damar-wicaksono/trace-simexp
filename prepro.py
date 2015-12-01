950882dfe24ae9d7254e34fa6a233ccb37287d57
WD41 <damar.wicaksono@psi.ch>
2015-12-01 17:19:11 +0100

    Rename files

---
+++ prepro.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from trace_simexp import prepro
+from trace_simexp import tracin
+
+__author__ = "Damar Wicaksono"
+
+
+def main():
+
+    # Construct a dictionary of required inputs from command line arguments, etc
+    inputs = prepro.get_input()
+
+    # Read list of parameters file and create a dictionary from it
+    params_dict = prepro.read_params(inputs["params_list_file"],
+                                     inputs["info_file"])
+
+    # Read the base tracin, obtain the nominal values, and update params_dict
+    tracin.get_nominal_values(inputs["tracin_base_file"], params_dict)
+
+    # Create a string template
+    tracin_template = tracin.create_template(params_dict,
+                                             inputs["tracin_base_file"])
+
+    # Read Design Matrix into a numpy array
+    dm = np.loadtxt(inputs["dm_file"])
+
+    # Create a directory structure based on
+    prepro.create_dirtree(inputs, params_dict, tracin_template, dm)
+
+
+if __name__ == "__main__":
+    main()
