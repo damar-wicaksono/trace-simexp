b9eced7168e0f2c42bd865f3a9aabae1a0de9530
Damar Wicaksono <damar.wicaksono@psi.ch>
2017-03-21 11:43:16 +0100

    Unify version string location across project

--- trace_simexp/__init__.py
+++ trace_simexp/__init__.py
@@ -4,6 +4,6 @@ from . import cmdln_args
 from . import info_file
 from . import util
 from . import template
+from ._version import __version__
 
 __author__ = "Damar Wicaksono"
-__version__ = "0.3.0"
