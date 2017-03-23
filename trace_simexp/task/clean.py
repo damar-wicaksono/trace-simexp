57e8cb59e56868d13e4755e260d962513566f6ca
Damar Wicaksono <damar.wicaksono@psi.ch>
2017-03-23 10:37:26 +0100

    Changed the argument for the shell `find` command to be in-between ' '
    In order to accomodate possibility of space for directory name

--- trace_simexp/task/clean.py
+++ trace_simexp/task/clean.py
@@ -1,4 +1,9 @@
-"""Module to clean up files and directories contents
+# -*- coding: utf-8 -*-
+"""
+    trace_simexp.task.clean
+    ***********************
+
+    Module to clean up files and directories contents
 """
 
 __author__ = "Damar Wicaksono"
@@ -7,7 +12,7 @@ __author__ = "Damar Wicaksono"
 def rm_files(files: list):
     """Remove the listed files
 
-    :param csv_files: (list) the list files or directories, fullname
+    :param csv_files: the list files or directories, fullname
     """
     import subprocess
     import os
@@ -22,15 +27,15 @@ def rm_files(files: list):
 
 
 def rm_except(directories: list, files: list):
-    """Remove the all the directory contents except the files
+    """Remove the all the directory contents except a single file
 
-    :param directories:
-    :param files:
+    :param directories: the list of directories
+    :param files: files within the directories not to be deleted
     """
     import subprocess
 
     for directory, file in zip(directories, files):
-        tmp_script = "find {} ! -name {} -type f -delete" .format(directory,
-                                                                   file)
+        tmp_script = "find '{}' ! -name {} -type f -delete" .format(directory,
+                                                                    file)
         # Use the whole string with shell interpreter
         subprocess.call(tmp_script, shell=True)
