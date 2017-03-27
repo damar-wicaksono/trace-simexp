dcc0f8e57fffe1c38ea499160d712ba6d23c5854
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-03-27 15:31:54 +0200

    Change sphinx-doc theme to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import shlex
+import sphinx_rtd_theme
 
 here = os.path.abspath(os.path.dirname(__file__))
 exec(open('../../trace_simexp/_version.py').read())
@@ -117,16 +118,16 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'show_related': True}
+#html_theme_options = {'show_related': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
